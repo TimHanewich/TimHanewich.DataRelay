15478174658e983f048f601c6563c81d536125e9
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-11 16:36:29 -0400

    Added functional test

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -7,6 +7,11 @@ namespace FunctionalTesting
     {
         static void Main(string[] args)
         {
+            test2();
+        }
+
+        static void test1()
+        {
             string msg = "Hello world!";
             DataPackage[] packages = DataPackage.CreateDataPackages(DataPackage.StringToBytes(msg), 5);
 
@@ -19,5 +24,15 @@ namespace FunctionalTesting
             Console.WriteLine(dps.AllPackagesReceived().ToString());
             Console.WriteLine(DataPackage.BytesToString(dps.GetCombinedContent()));
         }
+
+        static void test2()
+        {
+            string path = "C:\\Users\\tihanewi\\Downloads\\Formula 1 Games Telemetry\\Codemasters F1 2019 4-5-2020.txt";
+            string content = System.IO.File.ReadAllText(path);
+            DataPackage[] packs = DataPackage.CreateDataPackages(DataPackage.StringToBytes(content));
+            string s = DataPackage.BytesToString(DataPackage.GatherCombinedContent(packs));
+            Console.WriteLine(s);
+        }
+
     }
 }
